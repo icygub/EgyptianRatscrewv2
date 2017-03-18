fd19c0fe3a5c969ccc7fb0e5e2f920c3a621d348
icygub <jchubbs95@gmail.com>
2017-03-17 23:33:58 -0600

    The card class is now functional.

---
+++ src/sixpack/Hand.java
@@ -0,0 +1,13 @@
+package sixpack;
+
+/**
+ * Created by icyhot on 17/03/2017.
+ */
+public class Hand extends Deck {
+
+    public Hand() {
+        super();
+    }
+
+
+}
--- src/sixpack/Main.java
+++ src/sixpack/Main.java
@@ -1,8 +1,30 @@
 package sixpack;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
 
+
+
+//        System.out.print("Welcome to Egyptian Ratscrew v2! \n" +
+//                "What is your name? \n" +
+//                "> ");
+//        name = sc.nextLine();
+//
+//        System.out.printf("Hi, %s. The game will now start. \n" +
+//                "(enter key) = play card || (s + enter key) = slap", name);
+
+
+        //this tests the card class to make sure it works
+//        for(int i = 1; i <= 4; i++) {
+//            for(int j = 1; j <= 13; j++) {
+//                Card card = new Card(i,j);
+//                System.out.printf("%s of %s\n", card.getValue(),card.getSuit());
+//            }
+//        }
     }
 }
